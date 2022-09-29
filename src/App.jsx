6e1f82e383df99b0d51dbfb5fe0d0cbdf97337b8
manu <manuelibanezastorga@gmail.com>
2022-09-29 20:02:06 +0200

    agregamos bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='display-2'> pruebas</h1>
+      
+    </div>
+  );
+}
+
+export default App;
