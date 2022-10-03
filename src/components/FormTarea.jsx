965fda0c343d26756796a7cd1e440d4c6122000d
manu <manuelibanezastorga@gmail.com>
2022-10-03 11:25:37 -0300

    agregamos tareas para elform

--- src/components/FormTarea.jsx
+++ src/components/FormTarea.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import ListaTarea from "./ListaTarea"
+import ListaTarea from "./ListaTarea";
+
 const FormTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [arregloTareas, setArreglotareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArreglotareas([...arregloTareas, tarea]);
+    setTarea("");
+  };
+
   return (
     <div>
-      
-      <Form>
-        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-            Formcontrol es el input
-          <Form.Control type="text" placeholder="ingrese una tarea" />
-
-        <Button variant="primary" type="submit">
-          enviar
-        </Button>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
+          Formcontrol es el input
+          <Form.Control
+            type="text"
+            placeholder="ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">
+            enviar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTarea></ListaTarea>
